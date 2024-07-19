249aacd89615956a2bfb47f73621441661ad65cf
vishalkumar1007 <vishalkumarnke93@gmail.com>
2024-07-20 01:10:30 +0530

    50% work on sign up

--- src/page/SignUp/SignUp.jsx
+++ src/page/SignUp/SignUp.jsx
@@ -1,16 +1,89 @@
-import react from 'react'
-import { SafeAreaView , View, Text } from 'react-native';
-import { signUpStyles } from './SignUp_Style';
+import react from 'react';
+import {
+  Dimensions,
+  SafeAreaView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import {signUpStyles} from './SignUp_Style';
+import LOGO from '../../assets/PulsePlayLogo.png';
+const {width: WIDTH, height: HEIGHT} = Dimensions.get('window');
 
-const SignUp = ()=>{
-    return (
-        <SafeAreaView style={signUpStyles.main}>
-            <View>
-                <Text>
-                    We are done with initialized project on github
-                </Text>
-            </View>
-        </SafeAreaView>
-    )
-}
+const SignUp = () => {
+  console.log(`height : ${HEIGHT} and Width : ${WIDTH}`);
+
+  return (
+    <SafeAreaView style={signUpStyles.main}>
+      {/* <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={{flex: 1}}
+            > */}
+      <StatusBar animated={false} backgroundColor={'white'} />
+      {/* <ScrollView style={signUpStyles.scrollSignUpMain}> */}
+      <View style={signUpStyles.signUpMain}>
+        <View style={signUpStyles.logoMainDiv}>
+          <View style={signUpStyles.logoDiv}>
+            <Image style={signUpStyles.logo} source={LOGO} />
+          </View>
+        </View>
+        <View style={signUpStyles.welcome}>
+          <Text style={signUpStyles.welcomeText}>Welcome!</Text>
+          <Text style={signUpStyles.signUpText}>Create your account</Text>
+        </View>
+        <View style={signUpStyles.inputForm}>
+          <View style={signUpStyles.inputFormGap}>
+            <Text style={signUpStyles.inputLabel}>Name</Text>
+            <TextInput
+              style={signUpStyles.inputs}
+              placeholderTextColor="#a3a3a3"
+              placeholder="Enter your name"
+              Type='text'
+            />
+          </View>
+          <View style={signUpStyles.inputFormGap}>
+            <Text style={signUpStyles.inputLabel}>Email</Text>
+            <TextInput
+              style={signUpStyles.inputs}
+              placeholderTextColor="#a3a3a3"
+              placeholder="Enter email id"
+            />
+          </View>
+          <View style={signUpStyles.inputFormGap}>
+            <Text style={signUpStyles.inputLabel}>Password</Text>
+            <TextInput
+              style={signUpStyles.inputs}
+              placeholderTextColor="#a3a3a3"
+              placeholder="Enter Password"
+              secureTextEntry={true}
+              />
+          </View>
+          <View style={signUpStyles.inputFormGap}>
+            <Text style={signUpStyles.inputLabel}>Conform Password</Text>
+            <TextInput
+              style={signUpStyles.inputs}
+              placeholderTextColor="#a3a3a3"
+              placeholder="Again enter Password"
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+        <View style={signUpStyles.OrWith}>
+          <View style={signUpStyles.orLine}>
+            <View style={signUpStyles.orLineLeft}></View>
+            <Text style={signUpStyles.orLineText}>or</Text>
+            <View style={signUpStyles.orLineRight}></View>
+          </View>
+        </View>
+      </View>
+      {/* </ScrollView> */}
+      {/* </KeyboardAvoidingView> */}
+    </SafeAreaView>
+  );
+};
 export default SignUp;
--- src/page/SignUp/SignUp_Style.js
+++ src/page/SignUp/SignUp_Style.js
@@ -1,12 +1,121 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet , Dimensions} from "react-native";
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 
 export const signUpStyles = StyleSheet.create({
     main:{
-        backgroundColor:'orange',
-        width:'100%',
-        height:'100%',
+        backgroundColor:'white',
+        width:WIDTH,
+        height:HEIGHT,
         display:'flex',
         justifyContent:'center',
         alignItems:'center'
+    },
+    signUpMain:{
+        // backgroundColor:'green',
+        width:'85%',
+        height:'85%',
+        
+    },
+    scrollSignUpMain:{
+        // height:HEIGHT,
+        width:'100%',
+        
+    },
+    logoMainDiv:{
+        backgroundColor:'white',
+        width:'100%',
+        height:'12%',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    logoDiv:{
+        width:'70%',
+        height:'100%'
+    },
+    logo:{
+        width:'100%',
+        height:'100%'
+    },
+    welcome:{
+        backgroundColor:'white',
+        width:'100%',
+        height:'9%',
+        display:'flex',
+        justifyContent:'flex-end',
+        alignItems:'center'
+
+    },
+    welcomeText:{
+        color:'#131314', //black
+        fontSize:26,
+        fontWeight:`500`
+    },
+    signUpText:{
+        color:'#4a4a4a', //black gray
+        fontSize:14
+    },
+    inputForm:{
+        // backgroundColor:'blue',
+        width:'100%',
+        height:'61%',
+        paddingVertical: 15 ,
+        display:'flex',
+        // justifyContent:'center',
+        alignItems:'center',
+        gap:30,
+        paddingTop:40
+    },
+    inputFormGap:{
+        // backgroundColor:'pink',
+        width:'90%',
+        gap:10,
+
+    },
+    inputs:{
+        width:'100%',
+        height:45,
+        borderWidth:1,
+        borderColor:'#a3a3a3',
+        fontSize:18,
+        paddingHorizontal:10,
+        borderRadius:5,
+        color:'#333333'
+    },
+    inputLabel:{
+        color:'#2f2f2f',
+        fontSize:18,
+        fontWeight:`500`
+    },
+    OrWith:{
+        // backgroundColor:'pink',
+        width:'100%',
+        height:'16%'
+
+    },
+    orLine:{
+        // backgroundColor:'red',
+        width:'100%',
+        height:25,
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        gap:14
+    },
+    orLineLeft:{
+        width:'39%',
+        height:'8%',
+        backgroundColor:'#dedede'
+    },
+    orLineText:{
+        color:'#9b9999',
+        fontSize:15
+    },
+    orLineRight:{
+        width:'39%',
+        height:'8%',
+        backgroundColor:'#dedede'
+
     }
 })
